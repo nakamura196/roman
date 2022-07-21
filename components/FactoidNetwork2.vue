a179488ee5b46a138872eaef0f4a7395e45c9960
Satoru <you@example.com>
2022-07-21 17:43:40 +0900

    feat: add display function

--- components/FactoidNetwork2.vue
+++ components/FactoidNetwork2.vue
@@ -76,17 +76,21 @@ export default {
       ?s a/rdfs:subClassOf* ex:Factoid;
         ex:description ?desc . 
       ?s ?p ?ref . 
-      ?ref a/rdfs:subClassOf* ex:EntityReference;
-        ex:referencesEntity ?ref_referencesEntity;
-        ex:sourceDescription ?ref_sourceDescription . 
-      optional { ?ref ex:referencesEntityInContext ?ref_referencesEntityInContext } 
+      ?ref a/rdfs:subClassOf* ?er;
+        ex:referencesEntity ?referencesEntity;
+        ex:sourceDescription ?sourceDescription . 
+      optional {
+        ?referencesEntity ex:name ?referencesEntityName;
+        a ?referencesEntityType .
+      }
+      optional { ?ref ex:referencesEntityInContext ?referencesEntityInContext } 
       optional { 
         {
-          ?s ?v ?o . 
+          ?s ?v ?o . ?o a ?typeOfO .
         }
         UNION
         {
-          ?o ?v ?s . 
+          ?o ?v ?s . ?o a ?typeOfS .
         }
         ?o a/rdfs:subClassOf* ex:Factoid .
       }
@@ -98,6 +102,150 @@ export default {
 
     const { data } = await this.$axios.get(url)
     console.log({data})
+
+    const nodesMap = {}
+    const edgesMap = {}
+
+    for (const obj of data) {
+      if(obj.er === "https://junjun7613.github.io/RomanFactoid_v2/Roman_Contextual_Factoid.owl#EntityReference"){
+        continue
+      }
+      
+        const node = obj.s
+        // factoid
+        if (!nodesMap[node]) {
+          nodesMap[node] = {
+            id: node,
+            label: obj.desc,
+            shape: 'dot',
+            color: 'orange',
+            shadow: true,
+            original_color: 'orange',
+            type: 'factoid',
+          }
+        }
+
+        // referencesEntity
+        const referencesEntityNode = obj.referencesEntity
+        if (!nodesMap[referencesEntityNode]) {
+          const referencesEntityType = obj.referencesEntityType
+          console.log({referencesEntityType, obj})
+          // const localReferencesEntityType = referencesEntityType.split("#")[1]
+          let color = 'gray'
+          switch (referencesEntityType) {
+            case 'https://github.com/johnBradley501/FPO/raw/master/fpo.owl#Location':
+              // 式の結果が value1 に一致する場合に実行する文
+              color = '#98fb98'
+              break
+            case 'https://junjun7613.github.io/RomanFactoid_v2/Roman_Contextual_Factoid.owl#Community':
+              // 式の結果が value1 に一致する場合に実行する文
+              color = 'red'
+              break
+            case 'https://junjun7613.github.io/RomanFactoid_v2/Roman_Contextual_Factoid.owl#ConceptualObject':
+              // 式の結果が value1 に一致する場合に実行する文
+              color = 'yellow'
+              break
+            case 'https://junjun7613.github.io/RomanFactoid_v2/Roman_Contextual_Factoid.owl#PhysicalObject':
+              // 式の結果が value1 に一致する場合に実行する文
+              color = 'yellow'
+              break
+            case 'https://github.com/johnBradley501/FPO/raw/master/fpo.owl#Group':
+              // 式の結果が value1 に一致する場合に実行する文
+              color = 'orange'
+              break
+            case 'https://github.com/johnBradley501/FPO/raw/master/fpo.owl#Person':
+              // 式の結果が value1 に一致する場合に実行する文
+              color = '#03A9F4'
+              break
+          }
+          nodesMap[referencesEntityNode] = {
+            id: referencesEntityNode,
+            label: obj.referencesEntityName,
+            shape: 'dot',
+            color,
+            shadow: true,
+            original_color: color,
+          }
+        }
+
+        // factoidとreferencesEntity間のエッジ
+        const edge4referencesEntity = `${node}-${referencesEntityNode}`
+        edgesMap[edge4referencesEntity] = {
+          from: node,
+          to: referencesEntityNode,
+          // color: "blue",
+          color: 'gray',
+          arrows: {
+            to: {
+              enabled: true,
+              type: 'arrow',
+            },
+          },
+        }
+      
+
+      const nodeS = node // obj.s
+
+      if(obj.o){
+        const nodeO = obj.o
+
+      // factoid間のエッジ
+      const edge = `${nodeS}-${nodeO}`
+      
+      let from = null
+      let to = null
+      if(obj.typeOfO){
+        from = nodeS
+        to = nodeO
+      } else {
+        from = nodeO
+        to = nodeS
+      }
+
+      edgesMap[edge] = {
+        from,
+        to,
+        color: 'gray',
+        arrows: {
+          to: {
+            enabled: true,
+            type: 'arrow',
+          },
+        },
+      }
+      }
+      
+    }
+
+    /*
+    const res = await this.getAssociatedObjects(nodesMap, edgesMap)
+    nodesMap = res.nodesMap
+    edgesMap = res.edgesMap
+    */
+   
+    this.nodesMap = nodesMap
+    this.edgesMap = edgesMap
+
+    const nodes = []
+    const edges = []
+
+    for (const node in nodesMap) {
+      nodes.push(nodesMap[node])
+    }
+
+    for (const edge in edgesMap) {
+      edges.push(edgesMap[edge])
+    }
+
+    // this.nodes = nodes
+    // this.edges = edges
+
+    // 全データの格納
+    this.orgEdges = edges
+    this.orgNodes = nodes
+
+    // 描画
+    this.drawNetwork()
 
     // 2022-07-14
     // ここまで
